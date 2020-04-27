fb58ad85ca26fa6508b5a2b58bfbae697fb55849
Aavishkar Mishra <aavishkarmishra@gmail.com>
2020-04-27 15:50:26 +0530

    Added ADD,UPDATE,DELETE Routes for NOTIFICATION

--- README.md
+++ README.md
@@ -39,28 +39,119 @@ Follow the steps for working on this repository strictly :
 
 1. Locally, eg: http://localhost:9999/run-model/add_members
 2. With LIVE Heroku Server, eg: https://mcctezu-backend.herokuapp.com/run-model/add_members
-3. Test the API with POSTMAN. Example Input for add_members:  
+3. Test the API with POSTMAN. 
 
-{ 
+Example Input for add_members and edit members (ALL FIELDS MUST BE SENT):  
 
-    "_id": 1, 
-    "name": "Adittya Dey", 
-    "dept": "CSE", 
-    "linkedin": "somelink", 
-    "phone": "somenumber", 
-    "email": "someemail", 
-    "image": "somelink",
-    "interest": "Full Stack"
+```json
+    { 
+        "_id": 1, 
+        "name": "Adittya Dey", 
+        "dept": "CSE", 
+        "linkedin": "somelink", 
+        "phone": "somenumber", 
+        "email": "someemail", 
+        "image": "somelink",
+        "interest": "Full Stack"
+    }
+```
+
+Example Input for delete members (Send ID):  
+
+```json
+    { 
+        "_id": 1
+    }
+```
+
+
+Example for GET EVENTS :  
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/get_events`
+OR `https://mcctezu-backend.herokuapp.com/run-model/get_events/` to get all the records.
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/get_events/<event_name>` to get record for specified event only. 
+
+Example input for ADD EVENTS :
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/add_events`
+INPUT:
+```json
+    {
+        "evt_name":"Codeathon",
+        "evt_org":"MCC_TEZU",
+        "evt_date":"04/09/2020",
+        "evt_time":"3:00pm",
+        "evt_venue":"Dean's Building",
+        "evt_image":"none"
+    }
+```
+Example input for UPDATE EVENTS :
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/update_events`
+INPUT:
+```json
+    {
+        "field": {
+            "evt_name": "Code Hack"
+        },
+        "field_update": {
+            "evt_org": "MCC_TEZU"
+        }
+    }
+```
+In `field` attribute give the column name to identify the record to update. In `field_update` specify the column to be updated and give the new value to it.
+
+Example input for DELETE EVENTS :
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/delete_events`
+INPUT:
+```json
+    {
+    "evt_name":"Codathon"
+    }
+```
+Specify the column to identify and delete the record.
+
+Example input for ADD NOTIFICATIONS :
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/notifications/add`
+INPUT:
+```json
+    {
+        "date":"27/04/2020",
+        "title":"Events",
+        "notification":"sample text"
+    }
+```
+
+Example input for UPDATE EVENTS :
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/notifications/update_one` to Update Single Record OR
+Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/notifications/update_many` to Update More Than One Records
+
+INPUT:
+```json
+    {
+        "field": {
+            "title": "Events"
+        },
+        "update": {
+            "title": "Notifications"
+        }
+    }
+```
+In `field` attribute give the column name to identify the record to update. In `update` specify the column to be updated and give the new value to it.
+
+Example input for UPDATE EVENTS :
+* Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/notifications/delete_one` to Delete Single Record OR
+Set the URL TO `https://mcctezu-backend.herokuapp.com/run-model/notifications/delete_many` to Delete More Than One Records
+INPUT:
+```json
+    {
+
+        "date": "27/04/2020"
     
-}
+    }
+```
+<hr>
 
-<br> 
-
-### Author
+## Author
 
 #### [Adittya Dey](https://github.com/adiXcodr)
 
 [<img src="https://image.flaticon.com/icons/svg/185/185964.svg" width="35" padding="10">](https://www.linkedin.com/in/adittya-dey-3966b916b/)
-[<img src="https://image.flaticon.com/icons/svg/185/185981.svg" width="35" padding="10">](https://www.facebook.com/adittya.dey.3)
-[<img src="https://image.flaticon.com/icons/svg/185/185985.svg" width="35" padding="10">](https://www.instagram.com/adixdey/)
-
+[<img src="https://image.flaticon.com/icons/svg/185/185981.svg" width="35" padding="10">](https://www.facebook.com/adittya.d
--- run_model.py
+++ run_model.py
@@ -1,13 +1,14 @@
 from flask import Blueprint, jsonify, request
 import pymongo
 import constants as const
+import json
 
 bp = Blueprint("run_model", __name__)
 
 
 @bp.route("/add_members",methods=["POST"])
 def handle_add_members():
-    jn=request.json 
+    jn=request.json
     coll = const.mydb['members']
     x = coll.insert_one(jn)
     status_response = x.inserted_id
@@ -17,4 +18,161 @@ def handle_add_members():
         status_response='Failure'
     response = jsonify({"status": status_response,"data":"No data"})
     return response
+
+
+#-------------EVENTS HANDLER ROUTES-------------------
+
+@bp.route('/get_events',methods=['GET'])
+@bp.route('/get_events/',methods=['GET'])
+def get_event_handler():
+    event=const.mydb.events
+    query=event.find()
+    output=[]
+    if query:
+        for i in query:
+            output.append({"evt_org":i["evt_org"],"evt_name":i["evt_name"],"evt_date":i["evt_date"],"evt_time":i["evt_time"],"evt_venue":i["evt_venue"],"evt_image":i["evt_image"]})
+        status_response="Success"
+    else:
+        status_response="Failure"
+        output="No Record Found"
+    return jsonify({"status":status_response,"result":output})
+
+@bp.route('/get_events/<evt_name>',methods=['GET'])
+def get_event_handler_dynamic(evt_name):
+    event=const.mydb.events
+    i=event.find_one({"evt_name":evt_name})
+    output=[]
+    if i:
+        status_response='Success'
+        output.append({"evt_org":i["evt_org"],"evt_name":i["evt_name"],"evt_date":i["evt_date"],"evt_time":i["evt_time"],"evt_venue":i["evt_venue"],"evt_image":i["evt_image"]})
+    else:
+        status_response="Failure"
+        output="No Data Found"
+    return jsonify({"status":status_response,"result":output})
+
+@bp.route('/add_events',methods=['POST'])
+def add_events_handler():
+    event=const.mydb.events
+    q=request.json
+    query=event.find_one(q)
+    if query:
+        status_response="Failure"
+        output="Record Already Present"
+    else:
+        output="No Data"
+        event_id=event.insert_one(q)
+        status_response=event_id.inserted_id
+        if status_response:
+            status_response="Success"
+        else:
+            status_response="Failure"
+    return jsonify({"status": status_response,"result":output})
+
+@bp.route('/update_events',methods=['PUT'])
+def update_event_handler():
+    event=const.mydb.events
+    field=request.json["field"]
+    field_update=request.json['field_update']
+    if event.find_one(field):
+        output="No Data"
+        udt=event.update_one(field,{'$set':field_update})
+        if udt:
+            status_response="Success"
+        else:
+            status_response="Failure"
+    else:
+        status_response="Failure"
+        output="Record Not Found"
+    return jsonify({"status":status_response,"result":output})
+
+@bp.route('/delete_events',methods=['DELETE'])
+def delete_event_handler():
+    event=const.mydb.events
+    q=request.json
+    if event.find_one(q):
+        dlt=event.delete_one(q)
+        output='No Data'
+        if dlt:
+            status_response="Success"
+        else:
+            status_response="Failure"
+    else:
+        status_response="Failure"
+        output='No Record Found'
+    return jsonify({"status":status_response,"result":output})
     
+#-----------Notifications Routes-----------------
+
+@bp.route("/notifications/add",methods=["POST"])
+def add_notifications():
+    jn=request.json
+    notifications = const.mydb['notifications']
+    x = notifications.insert_one(jn)
+    status_response = x.inserted_id
+    print(status_response)
+    if(status_response):
+        status_response='Success'
+    else:
+        status_response='Failure'
+    response = jsonify({"status": status_response,"data":"No Data"})
+    return response
+
+@bp.route("/notifications/delete_one",methods=["DELETE"])
+def delete_one():
+    jn=request.json
+    notifications = const.mydb['notifications']
+    if(notifications.find_one(jn)):
+        status_response = notifications.delete_one(jn)
+        if(status_response):
+            status_response="Deleted"
+        else:
+            status_response="Failure"
+        return jsonify({"status":status_response,"data":"nodata"})
+    else:
+        return jsonify({"status":"No Record Found"})
+
+@bp.route("/notifications/delete_many",methods=["DELETE"])
+def delete_many():
+    jn=request.json
+    notifications = const.mydb['notifications']
+    if(notifications.find_one(jn)):
+        status_response = notifications.delete_many(jn)
+        if(status_response):
+            status_response="Deleted"
+        else:
+            status_response="Failure"
+        return jsonify({"status":status_response,"data":"nodata"})
+    else:
+        return jsonify({"status":"No Record Found"})
+
+
+@bp.route("/notifications/update_one",methods=["PUT"])
+def update_one():
+    field=request.json["field"]
+    update=request.json['update']
+    notifications = const.mydb['notifications']
+    if(notifications.find_one(field)):
+        status_response = notifications.update_one(field,{"$set":update})
+        if(status_response):
+            status_response="Updated"
+        else:
+            status_response="Failure"
+        return jsonify({"status":status_response,"data":"nodata"})
+    else:
+        return jsonify({"status":"No Record Found"})
+        
+@bp.route("/notifications/update_many",methods=["PUT"])
+def update_many():
+    field=request.json["field"]
+    update=request.json['update']
+    notifications = const.mydb['notifications']
+    if(notifications.find_one(field)):
+        status_response = notifications.update_many(field,{"$set":update})
+        if(status_response):
+            status_response="Updated"
+        else:
+            status_response="Failure"
+        return jsonify({"status":status_response,"data":"nodata"})
+    else:
+        return jsonify({"status":"No Record Found"})
+        
